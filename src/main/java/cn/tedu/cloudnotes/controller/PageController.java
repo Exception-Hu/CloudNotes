e5195395c82ebfd1b8230a7df536a5b6725cef4f
hujiyong <h15615633654@163.com>
2020-03-06 14:04:06 +0800

    pageController

---
+++ src/main/java/cn/tedu/cloudnotes/controller/PageController.java
@@ -0,0 +1,10 @@
+package cn.tedu.cloudnotes.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PageController {
+
+}
