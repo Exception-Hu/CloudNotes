2c73ee2feb8b4a9cba9f0f3ab5df0239054a95b7
hujiyong <h15615633654@163.com>
2020-03-06 12:31:04 +0800

    t

--- src/main/java/cn/tedu/cloudnotes/controller/JsonController.java
+++ src/main/java/cn/tedu/cloudnotes/controller/JsonController.java
@@ -19,30 +19,5 @@ public class JsonController {
 	public List<SelectJson> selectJson(){
 		return json.selectJson();
 	}
-	public void test(){
-	}
-	public void test2(){
-	}
-	public void test3(){
-	}
 	
-	public void test4() {
-		
-	}
-	public void test5(){
-	}
-	public void test8(){
-	}
-public void test7(){
-}
-public void test9() {
-	
-}
-
-public void test10() {
-	
-}
-
-public void test12(){}
-
 }
