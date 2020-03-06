6ecd1d0b17fb9e32d310d89d03f30106f1335513
Exception-Hu <54347983+Exception-Hu@users.noreply.github.com>
2020-03-06 11:50:16 +0800

    删除test

--- src/main/java/cn/tedu/cloudnotes/controller/JsonController.java
+++ src/main/java/cn/tedu/cloudnotes/controller/JsonController.java
@@ -19,14 +19,5 @@ public class JsonController {
 	public List<SelectJson> selectJson(){
 		return json.selectJson();
 	}
-	public void test(){
-	}
-	public void test2(){
-	}
-	public void test3(){
-	}
-	public void test5(){
-	}
-
 
 }
