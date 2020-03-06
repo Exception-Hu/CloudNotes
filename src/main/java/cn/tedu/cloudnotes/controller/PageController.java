dfbda0644dafdfe57e1786d795d77bdb39a1b476
hujiyong <h15615633654@163.com>
2020-03-06 11:08:50 +0800

    添加类

---
+++ src/main/java/cn/tedu/cloudnotes/controller/PageController.java
@@ -0,0 +1,12 @@
+package cn.tedu.cloudnotes.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PageController {
+	
+	public String page() {
+		return null;
+	}
+}
